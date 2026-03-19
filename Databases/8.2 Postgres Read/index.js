378c4a993da7331500a99632e5b6033a71c3f588
Makafui Ayegbe <makafuiayegbe@Makafuis-Mac-mini.local>
2026-03-19 18:11:27 -0400

    postgres

--- Databases/8.2 Postgres Read/index.js
+++ Databases/8.2 Postgres Read/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "user",
+  port: 5432
+})
 
 let totalCorrect = 0;
 
@@ -10,10 +19,26 @@ let totalCorrect = 0;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+db.connect();
+
 let currentQuestion = {};
 
-// GET home page
+let quiz = []
+
+db.query("SELECT * FROM flags", (err, res) => {
+    if(err){
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows
+  }
+  db.end();
+  })
+
+
+  // GET home page
 app.get("/", (req, res) => {
+
+  
   totalCorrect = 0;
   nextQuestion();
   console.log(currentQuestion);
@@ -24,7 +49,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
